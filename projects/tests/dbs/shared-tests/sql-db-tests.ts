6146b2771d7612317b859f15e66ad38995ccfa95
jycouet <jycouet@gmail.com>
2024-02-28 20:54:25 +0100

    tentative load test

--- projects/tests/dbs/shared-tests/sql-db-tests.ts
+++ projects/tests/dbs/shared-tests/sql-db-tests.ts
@@ -54,4 +54,63 @@ export function SqlDbTests({ createEntity, getRemult, getDb }: DbTestProps) {
       remult.dataProvider = dp
     }
   })
+
+  it('load dbNamesOf', async () => {
+    const dp = remult.dataProvider
+    try {
+      remult.dataProvider = getDb()
+      @Entity('e_load_d')
+      class d {
+        @Fields.number()
+        id = 0
+      }
+      @Entity('e_load_c')
+      class c {
+        @Fields.number()
+        id = 0
+        @Fields.number({
+          sqlExpression: async (x) => {
+            let db = await dbNamesOf(c)
+            let db2 = await dbNamesOf(d)
+            return `${db.id} + (SELECT ${db2.id} FROM ${db2.$entityName} LIMIT 1) + 1`
+          },
+        })
+        a = 0
+        @Fields.number({
+          sqlExpression: async (x) => {
+            let db = await dbNamesOf(c)
+            let db2 = await dbNamesOf(d)
+            return `${db.id} + (SELECT ${db2.id} FROM ${db2.$entityName} LIMIT 1) + 2`
+          },
+        })
+        b = 0
+      }
+      const e = await createEntity(c)
+      await e.insert({ id: 1 })
+      const f = await createEntity(d)
+      await f.insert({ id: 1 })
+
+      const res = await e.find()
+      expect(res).toMatchInlineSnapshot(`
+        [
+          c {
+            "a": 3,
+            "b": 4,
+            "id": 1,
+          },
+        ]
+      `)
+
+      const start = new Date().getTime()
+      await Promise.all(
+        Array.from({ length: 1000 }).map(async () => await e.count({ id: 1 })),
+      )
+
+      const duration = new Date().getTime() - start
+      // I don't know what could be a good value... I don't even know if it's a good test to have time involved.
+      expect(duration).toBeLessThan(10)
+    } finally {
+      remult.dataProvider = dp
+    }
+  })
 }
